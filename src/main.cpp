ed6c76bad286ba92ab76ff09ce99e3e5033fd57b
ttosi <ttosi519@gmail.com>
2022-07-07 11:22:22 -0700

    revert to original working

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,4 @@
 #include <Arduino.h>
-#include <EEPROM.h>
-#include "OneButton.h"
 #include "DRV8825.h"
 
 #define MOTOR_STEPS 200
@@ -14,9 +12,8 @@
 // 17320 degrees = 1 revolution on final output gear (maybe not)
 // 3 revolutions to close or open
 const long ONE_REV_DEGREES = 17320;
-const long NUM_REVS = 4;
+const long NUM_REVS = 3.55;
 const long TOTAL_REVS = NUM_REVS * ONE_REV_DEGREES;
-const long INCREMENT = .05;
 
 uint16_t sensorVoltage = 0;
 boolean isDay;
@@ -26,7 +23,6 @@ boolean hasRunNight = false;
 boolean ledState = false;
 
 DRV8825 motor(MOTOR_STEPS, DIR, STEP);
-OneButton button = OneButton(BUTTON_PIN, false, false);
 
 void flashLed(int count, int milliseconds)
 {
@@ -40,40 +36,6 @@ void flashLed(int count, int milliseconds)
   digitalWrite(LED_BUILTIN, LOW);
 }
 
-void openIncrement()
-{
-  Serial.println("open at increment");
-}
-
-void closeIncrement()
-{
-  Serial.println("close at increment");
-}
-
-void fullOpenClose()
-{
-  Serial.print("multi click: ");
-  Serial.println(button.getNumberClicks());
-
-  switch (button.getNumberClicks())
-  {
-  case 3:
-    Serial.println("full open");
-    break;
-  case 4:
-    Serial.println("full close");
-    break;
-  default:
-    Serial.println("invalid number of clicks");
-  }
-}
-
-void saveChangesToEeprom()
-{
-  Serial.println("save changes to eeprom");
-  flashLed(10, 100);
-}
-
 void setup()
 {
   Serial.begin(9600);
@@ -81,15 +43,10 @@ void setup()
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(LDR_PIN, INPUT);
 
-  button.attachClick(openIncrement);
-  button.attachDoubleClick(closeIncrement);
-  button.attachMultiClick(fullOpenClose);
-  button.attachLongPressStop(saveChangesToEeprom);
-
   motor.begin(RPM);
   motor.setMicrostep(8);
 
-  Serial.println("setup complete");
+  // Serial.println("setup complete");
 }
 
 void loop()
@@ -97,8 +54,8 @@ void loop()
   sensorVoltage = analogRead(LDR_PIN);
   // Serial.println(sensorVoltage);
 
-  isDay = sensorVoltage > 700 ? true : false;
-  isNight = sensorVoltage < 150 ? true : false;
+  isDay = sensorVoltage > 750 ? true : false;
+  isNight = sensorVoltage < 50 ? true : false;
 
   // open the blinds
   if (isDay && !hasRunDay)
@@ -106,11 +63,10 @@ void loop()
     digitalWrite(LED_BUILTIN, HIGH);
 
     motor.enable();
-    motor.rotate(ONE_REV_DEGREES * 3.52);
+    motor.rotate(NUM_REVS);
     motor.disable();
 
-    digitalWrite(LED_BUILTIN, LOW);
-
+    flashLed(10, 100);
     hasRunDay = true;
     hasRunNight = false;
   }
@@ -121,15 +77,13 @@ void loop()
     digitalWrite(LED_BUILTIN, HIGH);
 
     motor.enable();
-    motor.rotate(-ONE_REV_DEGREES * 3.52);
+    motor.rotate(-NUM_REVS);
     motor.disable();
 
-    digitalWrite(LED_BUILTIN, LOW);
-
+    flashLed(10, 100);
     hasRunDay = false;
     hasRunNight = true;
   }
 
-  button.tick();
-  delay(50);
+  delay(250);
 }
